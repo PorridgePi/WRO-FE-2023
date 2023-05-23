82e957cc600529dbd9dfe804b76d11ec2cf837e1
PorridgePi <kunbao2006@126.com>
2023-05-23 16:42:52 +0800

    feat: Add Definitions.h

---
+++ lib/private/Definitions.h
@@ -0,0 +1,30 @@
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+
+#define PIN_WIRE0_SCL 13
+#define PIN_WIRE0_SDA 12
+
+#define PIN_WIRE1_SCL 15
+#define PIN_WIRE1_SDA 14
+
+#define PIN_MOTOR_A 18
+#define PIN_MOTOR_B 19
+
+#define PIN_ENCODER_A 20
+#define PIN_ENCODER_B 21
+
+#define PIN_SERVO 22
+
+// Below for Protoboard
+
+// #define PIN_MOTOR_A 17
+// #define PIN_MOTOR_B 16
+// #define PIN_MOTOR_PWM 21
+// #define PIN_MOTOR_EN 27
+
+// #define PIN_ENCODER_A 15
+// #define PIN_ENCODER_B 14
+
+// #define PIN_SERVO 22
+
+#endif
