3127a636a8881776f3dea2cf90d1e0568f4fa68e
PorridgePi <kunbao2006@126.com>
2023-08-04 14:50:08 +0800

    feat(test): Add test code for motor and encoder combined

---
+++ src/test/pico_lib_motor_encoder_pcb.cpp
@@ -0,0 +1,39 @@
+#include <Arduino.h>
+#include <CommonUtils.h>
+#include <Definitions.h>
+#include <Encoder.h>
+#include <Motor.h>
+#include <Button.h>
+
+Encoder encoder(PIN_ENCODER_A, PIN_ENCODER_B);
+Button button(PIN_BUTTON_A, PIN_BUTTON_B);
+Motor motor(PIN_MOTOR_A, PIN_MOTOR_B);
+
+void setup() {
+    Serial.begin(115200);
+}
+
+void loop() {
+    float speed;
+    float sumAngle = encoder.read() / 4.0f;
+    float displacement = sumAngle / 360.0f * 2 * PI * WHEEL_DIAMETER;
+
+    encoder.update();
+
+    Serial.print("Angle Sum: ");
+    Serial.print(sumAngle);
+    Serial.print(" Displacement: ");
+    Serial.print(displacement);
+    Serial.println();
+
+    if (button.isPressed()) {
+        speed = 0.5;
+    } else {
+        speed = 0;
+    }
+    motor.setSpeed(speed);
+}
+
+void loop1() {
+    blinkLED();
+}
