31be250138689ac4c1755794f9ee81c1a813fb50
PorridgePi <kunbao2006@126.com>
2023-08-11 14:34:23 +0800

    feat(test): Add different kinds of servo movements

--- src/test/pico_lib_driveservo.cpp
+++ src/test/pico_lib_driveservo.cpp
@@ -3,8 +3,25 @@
 #include <CommonUtils.h>
 #include <DriveServo.h>
 
-DriveServo servo(PIN_SERVO);
+DriveServo servo(PIN_SERVO, 1000, 1500, 2000);
 int pos = 0;
+
+void sineMotion() {
+    float pos = sin(millis() / 3000.0);
+    Serial.println(pos);
+    servo.turn(pos);
+}
+
+void fixedMotion() {
+    servo.turn(0);
+    delay(2000);
+    servo.turn(1);
+    delay(2000);
+    servo.turn(0);
+    delay(2000);
+    servo.turn(-1);
+    delay(2000);
+}
 
 void setup() {
     Serial.begin(115200);
@@ -12,9 +29,8 @@ void setup() {
 }
 
 void loop() {
-    float pos = sin(millis() / 2000.0);
-    Serial.println(pos);
-    servo.turn(pos);
+    fixedMotion();`
+    // sineMotion();
 }
 
 void loop1() {
