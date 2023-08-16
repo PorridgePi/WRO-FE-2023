f5029c1db471a321cea621b656984b7c868f5c96
PorridgePi <kunbao2006@126.com>
2023-08-16 15:27:14 +0800

    feat: Update main code with new encoder and turning

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,59 @@
 #include <Motor.h>
 #include <DriveServo.h>
 #include <MechaQMC5883.h>
+#include <Lidar.h>
+
+
+volatile int ticks = 0;
+
+void checkEncoder() {
+  if (digitalRead(PIN_ENCODER_A) == digitalRead(PIN_ENCODER_B)) {
+    ticks++;
+  } else {
+    ticks--;
+  }
+}
+
 
 #define DEBUG_PRINT true
+#define DIRECTION -1
+#define SPEED 0.5
 
 Button button(PIN_BUTTON_A, PIN_BUTTON_B);
 Motor motor(PIN_MOTOR_A, PIN_MOTOR_B);
 DriveServo servo(PIN_SERVO);
-MechaQMC5883 imu(Wire, -360, -354, 0.868168186471, -31.5296782652);
+MechaQMC5883 imu(Wire, 569.5, 89.5, 0.938978508806, 14.7584706712);
+Lidar frontLidar(Wire, 0x10);
+// Encoder encoder(PIN_ENCODER_A, PIN_ENCODER_B);
 
 float speed = 0, turnRatio = 0;
 int currentCase = 0;
+int encoderCount = 0;
+
+bool turn(float angle) {
+    static float targetAngle = -1;
+    if (targetAngle == -1) {
+        targetAngle = LIM_ANGLE(imu.readAngle() + 90);
+    }
+
+    float currentAngle = imu.readAngle();
+
+    float turnAngle = ANGLE_360_TO_180(LIM_ANGLE(targetAngle - currentAngle));
+
+    DPRINT(turnAngle);
+
+    if (turnAngle > -1) {
+        turnRatio = turnAngle > 0 ? 1 : -1;
+        speed = SPEED;
+        DPRINT(currentAngle);
+        DPRINT(targetAngle);
+        DPRINT(turnRatio);
+        return false;
+    } else {
+        targetAngle = -1;
+        return true;
+    }
+}
 
 void setupComponents() {
     // I2C for LiDAR and IMU
@@ -33,6 +76,15 @@ void setupComponents() {
     // IMU
     imu.init();
     imu.tare();
+
+    // Encoder
+    attachInterrupt(digitalPinToInterrupt(PIN_ENCODER_A), checkEncoder, CHANGE);
+}
+
+void update() {
+    // encoder.update();
+    // encoderCount = encoder.read() * DIRECTION;
+    encoderCount = ticks * DIRECTION;
 }
 
 void setup() {
@@ -41,42 +93,52 @@ void setup() {
 }
 
 void loop() {
+    update();
     static float targetAngle = -1;
+    speed = SPEED;
+    static int sideCount = 0;
+
     switch (currentCase) {
     case 0: // stop
         speed = 0;
         turnRatio = 0;
+        sideCount = 0;
         if (button.isPressed()) {
             currentCase = 1;
         }
         break;
     case 1:
-        speed = -50.5;
+        speed = SPEED;
         turnRatio = 0;
-
-        currentCase = 2;
+        if (frontLidar.read() < 40) {
+            currentCase = 2;
+        }
         break;
     case 2:
-        if (targetAngle == -1) {
-            targetAngle = LIM_ANGLE(imu.readAngle() + 90);
+        if (turn(90)) {
+            sideCount++;
+            if (sideCount < 4) {
+                currentCase = 1;
+            } else {
+                currentCase = 0;
+            }
         }
-        if (imu.readAngle() < targetAngle) {
-            turnRatio = 1;
-            speed = 0.5;
-        } else {
-            targetAngle = -1;
-            currentCase = 0;
-        }
+        break;
     }
 
     DPRINT(currentCase);
-    EPRINT(imu.readAngle());
+    DPRINT(frontLidar.read());
+
+    DPRINT(encoderCount);
+
+    DPRINT(imu.readAngle());
+    DPRINT(speed);
     DPRINT(turnRatio);
-    DPRINT(targetAngle);
+
     Serial.println();
 
-    servo.turn(turnRatio);
-    motor.setSpeed(speed);
+    servo.turn(turnRatio * DIRECTION);
+    motor.setSpeed(speed * DIRECTION);
 }
 
 void loop1() {
