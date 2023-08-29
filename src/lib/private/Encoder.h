63df6048bb4043f486bfe56169b888fb54d4f38e
PorridgePi <kunbao2006@126.com>
2023-08-29 09:40:50 +0800

    feat(lib): Updated Encoder library using interrupt

--- src/lib/private/Encoder.h
+++ src/lib/private/Encoder.h
@@ -5,34 +5,40 @@
 
 class Encoder {
     public:
-        Encoder(int pinA, int pinB) : _pinA(pinA), _pinB(pinB) {
+        Encoder(int pinA, int pinB, bool reverseDirection = false, float wheelDiameter, float resolution = 4.0) : _reverseDirection(reverseDirection), _wheelDiameter(wheelDiameter), _resolution(resolution) {
+            // static variables can't be initialized in the constructor
+            _pinA = pinA;
+            _pinB = pinB;
+
             pinMode(_pinA, INPUT);
             pinMode(_pinB, INPUT);
-            _aLastState = digitalRead(_pinA);
+
+            attachInterrupt(digitalPinToInterrupt(_pinA), checkEncoder, CHANGE);
         }
 
-        void update() {
-            _aState = digitalRead(_pinA);
-            if (_aState != _aLastState) {
-                if (digitalRead(_pinB) != _aState) { // if B differs from A, rotating clockwise
-                    _counter++;
-                } else { // else, rotating counter-clockwise
-                    _counter--;
-                }
-            }
-            _aLastState = _aState;
+        int readCounter() {
+            return _counter * (_reverseDirection ? -1 : 1);
         }
 
-        long long read() {
-            return _counter;
+        float readDistance() {
+            return readCounter() / _resolution / 360.0f * _wheelDiameter * PI;
         }
 
     private:
-        int _pinA;
-        int _pinB;
-        int _aState;
-        int _aLastState;
-        long long _counter = 0;
+        bool _reverseDirection;
+        float _wheelDiameter;
+        float _resolution;
+        static int _pinA;
+        static int _pinB;
+        volatile static int _counter = 0;
+
+        static void checkEncoder() {
+            if (digitalRead(_pinA) == digitalRead(_pinB)) {
+                _counter++;
+            } else {
+                _counter--;
+            }
+        }
 };
 
 #endif
