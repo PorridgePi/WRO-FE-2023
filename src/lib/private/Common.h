c698e5b9a280ca2a5f7fae77dc066866c3683ec7
PorridgePi <kunbao2006@126.com>
2023-08-31 13:14:56 +0800

    fix: Update Common.h - cleanup, organise - add comments - update IMU calibration - add DIST_BETWEEN_TWO_LIDARS definition - fix!: distances using distLeft instead of correct sides - rename variables to indicate that they are temporary

--- src/lib/private/Common.h
+++ src/lib/private/Common.h
@@ -14,11 +14,12 @@
 #define LIDAR_DIFF_CORRECTION_LEFT -4.0
 #define LIDAR_DIFF_CORRECTION_RIGHT -4.0
 #define TURNING_RADIUS 20
+#define DIST_BETWEEN_TWO_LIDARS 13
 
 Button button(PIN_BUTTON_A, PIN_BUTTON_B);
 Motor motor(PIN_MOTOR_A, PIN_MOTOR_B);
 DriveServo servo(PIN_SERVO);
-MechaQMC5883 imu(Wire, -28, 46.5, 1.35667791828, -89.8894438815);
+MechaQMC5883 imu(Wire, -7.5, 64, 1.33294029345, 89.5173413224);
 Lidar lidarFront(Wire, 0x10);
 Lidar lidarLeft(Wire1, 0x11);
 Lidar lidarLeftBack(Wire1, 0x10);
@@ -29,37 +30,38 @@ Encoder encoder(PIN_ENCODER_A, PIN_ENCODER_B, 6.5);
 #define WALL_PRESENT_DISTANCE 35 // if lower than this, wall is present
 #define WALL_MISSING_DISTANCE 100 // if higher than this, wall is missing
 
+// Movement Control Variables
 float speed = 0, turnRatio = 0;
 
+// Case Variables
 int currentSide = 0; // clockwise, side 0 = heading 0, side 1 = heading 90, side 2 = heading 180, side 3 = heading 270
 int caseMain = 0;
+bool isClockwise = true; // clockwise -> turn right, anticlockwise -> turn left
+int cornerCount = 0;
+
+// Sensor Variables
+//// Encoder
 float encoderDistance = 0;
+
+//// LiDAR
+float initialDistLeft = 0, initialDistRight = 0, initialDistFront = 0;
 
 float distLeft = 0, distRight = 0, distFront, distLeftBack, distRightBack = 0;
 float distLeftCorr = 0, distRightCorr = 0, distFrontCorr = 0, distLeftBackCorr = 0, distRightBackCorr = 0;
-float innerDist = 0, innerDistCorr = 0, outerDist = 0, outerDistCorr = 0, innerDistBack = 0, innerDistBackCorr = 0, outerDistBack = 0, outerDistBackCorr = 0;
+float innerDist = 0, innerDistCorr = 0, innerDistBack = 0, innerDistBackCorr = 0;
+float outerDist = 0, outerDistCorr = 0, outerDistBack = 0, outerDistBackCorr = 0;
 
-int initialDistLeft = 0, initialDistRight = 0, initialDistFront = 0;
+float lidarHeading = 0;
 
-float headingDiff, lidarHeading;
-
+//// IMU
 float trueAngle = 0, trueAngleZeroError = 0; // relative to the start i.e. 0 <= x < 360
 float relativeAngle = 0, relativeAngleZeroError = 0; // relative to each side i.e. 0 <= x < 90
 
-bool isClockwise = true; // clockwise -> turn right, anticlockwise -> turn left
-
-int cornerCount = 0;
+//// Misc
+float headingDiff = 0;
 
 void correctToRelativeZero() {
     turnRatio = -1 * constrain(ANGLE_360_TO_180(relativeAngle) / 30, -1, 1);
-    // if (abs(turnRatio) < 0.04) { // 0.04 * 30 = 1.2 degrees
-    //     turnRatio = 0;
-    // } else {
-    //     const float MIN_TURN_RATIO = 0.3;
-    //     if (abs(turnRatio) < MIN_TURN_RATIO) {
-    //         turnRatio = turnRatio > 0 ? MIN_TURN_RATIO : -1 * MIN_TURN_RATIO;
-    //     }
-    // } 
 }
 
 void relativeAngleTare(float diff = 0) {
@@ -73,26 +75,26 @@ void trueAngleZeroTare(float diff = 0) {
 void update() {
     encoderDistance = encoder.readDistance();
 
-    float angle = imu.readAngle();
+    float tempAngle = imu.readAngle();
+    trueAngle = LIM_ANGLE(tempAngle - trueAngleZeroError);
     relativeAngle = LIM_ANGLE(trueAngle - currentSide * 90);
-    trueAngle = LIM_ANGLE(angle - trueAngleZeroError);
 
-    int tempLeft, tempRight, tempFront, tempLeftBack, tempRightBack;
-    tempLeft = lidarLeft.read();
-    tempRight = lidarRight.read();
-    tempFront = lidarFront.read();
-    tempLeftBack = lidarLeftBack.read();
-    tempRightBack = lidarRightBack.read();
+    int tempDistLeft, tempDistRight, tempDistFront, tempDistLeftBack, tempDistRightBack;
+    tempDistLeft = lidarLeft.read();
+    tempDistRight = lidarRight.read();
+    tempDistFront = lidarFront.read();
+    tempDistLeftBack = lidarLeftBack.read();
+    tempDistRightBack = lidarRightBack.read();
 
-    if (tempLeft == 0 || tempRight == 0 || tempFront == 0 || tempLeftBack == 0 || tempRightBack == 0) {
+    if (tempDistLeft == 0 || tempDistRight == 0 || tempDistFront == 0 || tempDistLeftBack == 0 || tempDistRightBack == 0) {
         // EPRINT("LIDAR ERROR");
     }
 
-    distLeft = tempLeft <= 0 ? distLeft : tempLeft;
-    distRight = tempRight <= 0 ? distLeft : tempRight;
-    distFront = tempFront <= 0 ? distLeft : tempFront;
-    distLeftBack = tempLeftBack <= 0 ? distLeft : tempLeftBack;
-    distRightBack = tempRightBack <= 0 ? distLeft : tempRightBack;
+    distLeft = tempDistLeft <= 0 ? distLeft : tempDistLeft;
+    distRight = tempDistRight <= 0 ? distRight : tempDistRight;
+    distFront = tempDistFront <= 0 ? distFront : tempDistFront;
+    distLeftBack = tempDistLeftBack <= 0 ? distLeftBack : tempDistLeftBack;
+    distRightBack = tempDistRightBack <= 0 ? distRightBack : tempDistRightBack;
 
     distLeftCorr = distLeft * cos(RAD(ANGLE_360_TO_180(relativeAngle)));
     distRightCorr = distRight * cos(RAD(ANGLE_360_TO_180(relativeAngle)));
@@ -100,19 +102,21 @@ void update() {
     distLeftBackCorr = distLeftBack * cos(RAD(ANGLE_360_TO_180(relativeAngle)));
     distRightBackCorr = distRightBack * cos(RAD(ANGLE_360_TO_180(relativeAngle)));
 
-    innerDistCorr = isClockwise ? distRightCorr : distLeftCorr;
     innerDist = isClockwise ? distRight : distLeft;
-    outerDistCorr = isClockwise ? distLeftCorr : distRightCorr;
-    outerDist = isClockwise ? distLeft : distRight;
+    innerDistCorr = isClockwise ? distRightCorr : distLeftCorr;
     innerDistBackCorr = isClockwise ? distRightBackCorr : distLeftBackCorr;
     innerDistBack = isClockwise ? distRightBack : distLeftBack;
-    EPRINT(innerDist - innerDistBack);
+    outerDist = isClockwise ? distLeft : distRight;
+    outerDistCorr = isClockwise ? distLeftCorr : distRightCorr;
+
     if (isClockwise) {
-        lidarHeading = LIM_ANGLE(360 - DEG(atan2(innerDist - innerDistBack - LIDAR_DIFF_CORRECTION_RIGHT, 13)));
+        lidarHeading = LIM_ANGLE(360 - DEG(atan2(innerDist - innerDistBack - LIDAR_DIFF_CORRECTION_RIGHT, DIST_BETWEEN_TWO_LIDARS)));
     } else {
-        lidarHeading = LIM_ANGLE(DEG(atan2(innerDist - innerDistBack - LIDAR_DIFF_CORRECTION_LEFT, 13)));
+        lidarHeading = LIM_ANGLE(DEG(atan2(innerDist - innerDistBack - LIDAR_DIFF_CORRECTION_LEFT, DIST_BETWEEN_TWO_LIDARS)));
     }
+
     headingDiff = ANGLE_360_TO_180(DELTA_ANGLE(relativeAngle, lidarHeading));
+
     // DPRINT(innerDist);
     // DPRINT(distRightBack);
     // DPRINT(lidarHeading);
@@ -120,32 +124,31 @@ void update() {
     // DPRINT(headingDiff);
 }
 
-void turn(float angle, float direction = 0) { // direction = 0 -> turn shortest way, direction = 1 -> turn clockwise, direction = -1 -> turn anticlockwise
-    digitalWrite(PIN_LED, HIGH);
+void turn(float angle, float direction = 0.0f, float error = 1.0f) { // direction = 0 -> turn shortest way, direction = 1 -> turn clockwise, direction = -1 -> turn anticlockwise
+    const float MIN_TURN_RATIO = 0.5;
+
     update();
     float targetAngle = LIM_ANGLE(trueAngle + angle);
-    while (abs(trueAngle - targetAngle) > 1) {
+    while (abs(trueAngle - targetAngle) > error) {
         update();
         digitalWrite(PIN_LED, HIGH);
         turnRatio = constrain(ANGLE_360_TO_180(DELTA_ANGLE(trueAngle, targetAngle))/30.0f, -1, 1);
         if (direction != 0) {
             turnRatio = abs(turnRatio) * direction;
         }
-        const float MIN_TURN_RATIO = 0.5;
         if (abs(turnRatio) < MIN_TURN_RATIO) {
             turnRatio = turnRatio > 0 ? MIN_TURN_RATIO : -1 * MIN_TURN_RATIO;
         }
+
         EPRINT("turning...");
         DPRINT(trueAngle);
         DPRINT(targetAngle);
         EPRINT(DELTA_ANGLE(trueAngle, targetAngle));
         Serial.println();
     }
-    digitalWrite(PIN_LED, LOW);
 }
 
 void moveStraight(float distance, float speed = SPEED) {
-    digitalWrite(PIN_LED, HIGH);
     update();
     float targetDistance = encoderDistance + distance;
     while (abs(encoderDistance - targetDistance) > 0.5) {
@@ -160,7 +163,6 @@ void moveStraight(float distance, float speed = SPEED) {
         DPRINT(targetDistance);
         Serial.println();
     }
-    digitalWrite(PIN_LED, LOW);
 }
 
 void setupComponents() {
