1061d397e24dfe2999ede4785094adb41b4fb2a0
PorridgePi <kunbao2006@126.com>
2023-09-05 13:50:53 +0800

    chore: Update IMU calibration

--- src/test/pico_imu_calibration.cpp
+++ src/test/pico_imu_calibration.cpp
@@ -6,7 +6,7 @@
 
 // Use in combination with https://www.desmos.com/calculator/jvlm9fmemt
 
-MechaQMC5883 imu(Wire, 165, 60, 1.28710882619, -88.1088452983);
+MechaQMC5883 imu(Wire, 121, 68, 1.2336059744, 88.6456235557);
 // MechaQMC5883 imu(Wire, 0, 0, 0, 0);
 MechaQMC5883 imu1(Wire1, -96, -221, 1.95970583558, 48.003770227);
 // MechaQMC5883 imu1(Wire1, 0, 0, 0, 0);
